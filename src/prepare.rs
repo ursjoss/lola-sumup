2d73bec53f8a1abfa166db3935e56fdc063c9db1
Urs Joss <urs.joss@gmx.ch>
2023-05-16 23:10:42 +0200

    refactor: from raw source to intermediate with polars instead of csv

--- src/prepare.rs
+++ src/prepare.rs
@@ -4,104 +4,165 @@ use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::{error::Error, fmt, io};
 
-use chrono::{NaiveDate, NaiveTime};
+use polars::datatypes::DataType;
+use polars::frame::{DataFrame, UniqueKeepStrategy};
+use polars::io::{SerReader, SerWriter};
+use polars::prelude::{
+    col, lit, when, CsvReader, CsvWriter, Expr, IntoLazy, JoinType, LazyFrame, StrptimeOptions,
+};
 use serde::{Deserialize, Serialize};
 
 /// Prepares the intermediate working copy of the original file.
 /// Some derived fields are prepared in a best-effort approach.
-/// The values can optionally be tweaked until it matches the expectations.
+/// The values of the intermediate file can (manually) be optionally tweaked until it matches the expectations.
 pub fn prepare(input_path: &Path, output_path: &Option<PathBuf>) -> Result<(), Box<dyn Error>> {
+    let mut df = read_data(input_path)?;
     let iowtr: Box<dyn Write> = match output_path {
         Some(path) => Box::new(File::create(path)?),
         None => Box::new(io::stdout()),
     };
-    let mut wtr = csv::WriterBuilder::new().delimiter(b';').from_writer(iowtr);
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .from_path(input_path)?;
-
-    let mut all_trx: Vec<Intermediate> = Vec::new();
-    for result in rdr.deserialize() {
-        let record_in: SalesReport = result?;
-        all_trx.push(From::from(record_in));
-    }
-
-    let ids_of_refunded = get_ids_of_refunds(&all_trx);
-
-    for trx in all_trx {
-        if is_not_refunded(&ids_of_refunded, &trx) {
-            wtr.serialize(&trx)?;
-        }
-    }
-    wtr.flush()?;
+    CsvWriter::new(iowtr)
+        .has_header(true)
+        .with_delimiter(b';')
+        .with_date_format(Some("%d.%m.%Y".into()))
+        .with_time_format(Some("%H:%M:%S".into()))
+        .finish(&mut df)?;
     Ok(())
 }
 
-fn get_ids_of_refunds(records: &[Intermediate]) -> Vec<String> {
-    records
-        .iter()
-        .map(|r| r.transaction_refunded.clone())
-        .filter(|id| !id.is_empty())
-        .filter(|id| contains_id(records, id))
-        .collect()
+fn read_data(input_path: &Path) -> Result<DataFrame, Box<dyn Error>> {
+    let raw_df = CsvReader::from_path(input_path)?
+        .has_header(true)
+        .with_delimiter(b',')
+        .finish()?;
+    build_df(&raw_df)
 }
 
-fn contains_id(records: &[Intermediate], id: &String) -> bool {
-    records
-        .iter()
-        .filter(|r| r.transaction_id == *id)
-        .peekable()
-        .peek()
-        .is_some()
+fn build_df(raw_df: &DataFrame) -> Result<DataFrame, Box<dyn Error>> {
+    let raw_date_format = StrptimeOptions {
+        format: Some("%d.%m.%y".into()),
+        strict: true,
+        exact: true,
+        ..Default::default()
+    };
+    let time_format = StrptimeOptions {
+        format: Some("%H:%M:%S".into()),
+        strict: true,
+        exact: true,
+        ..Default::default()
+    };
+
+    let (refunded_ids_1, refunded_ids_2) = refunded_id_dfs(raw_df);
+
+    let df = raw_df
+        .clone()
+        .lazy()
+        .with_column(
+            col("Date")
+                .str()
+                .strptime(DataType::Date, raw_date_format)
+                .alias("Date"),
+        )
+        .with_column(
+            (col("Time") + lit(":00"))
+                .str()
+                .to_time(time_format.clone())
+                .alias("Time"),
+        )
+        .with_column(col("Description").str().strip(None).alias("Description"))
+        .with_column(infer_topic(time_format).alias("Topic"))
+        .with_column(lit("").alias("Comment"))
+        .join(
+            refunded_ids_1,
+            [col("Transaction ID")],
+            [col("TransactionId")],
+            JoinType::Left,
+        )
+        .join(
+            refunded_ids_2,
+            [col("Transaction refunded")],
+            [col("TransactionId")],
+            JoinType::Left,
+        )
+        .filter(col("refunded1").is_null().and(col("refunded2").is_null()))
+        .select([
+            col("Account"),
+            col("Date"),
+            col("Time"),
+            col("Type"),
+            col("Transaction ID"),
+            col("Receipt Number"),
+            col("Payment Method"),
+            col("Quantity"),
+            col("Description"),
+            col("Currency"),
+            col("Price (Gross)"),
+            col("Price (Net)"),
+            col("Tax"),
+            col("Tax rate"),
+            col("Transaction refunded"),
+            col("Topic"),
+            infer_owner().alias("Owner"),
+            infer_purpose().alias("Purpose"),
+            col("Comment"),
+        ])
+        .collect()?;
+    Ok(df)
 }
 
-fn is_not_refunded(refunded: &[String], record_out: &Intermediate) -> bool {
-    !refunded.contains(&record_out.transaction_id)
-        && !refunded.contains(&record_out.transaction_refunded)
+fn refunded_id_dfs(raw_df: &DataFrame) -> (LazyFrame, LazyFrame) {
+    let transaction_ids = raw_df.clone().lazy().select([col("Transaction ID")]);
+    let ids_of_refunded = raw_df
+        .clone()
+        .lazy()
+        .filter(col("Transaction refunded").is_not_null())
+        .select([col("Transaction refunded")])
+        .join(
+            transaction_ids,
+            [col("Transaction refunded")],
+            [col("Transaction ID")],
+            JoinType::Inner,
+        )
+        .select([col("Transaction refunded")])
+        .unique(None, UniqueKeepStrategy::First);
+    let refunded_ids_1 = ids_of_refunded
+        .with_column(col("Transaction refunded").alias("TransactionId"))
+        .with_column(lit("x").alias("refunded1"))
+        .select([col("TransactionId"), col("refunded1")]);
+    let refunded_ids_2 = refunded_ids_1
+        .clone()
+        .select([col("TransactionId"), col("refunded1").alias("refunded2")]);
+    (refunded_ids_1, refunded_ids_2)
 }
 
-mod parse_date {
-    use chrono::NaiveDate;
-    use serde::{self, Deserialize, Deserializer, Serializer};
-
-    #[allow(clippy::trivially_copy_pass_by_ref)]
-    pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = format!("{}", date.format("%d.%m.%Y"));
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        NaiveDate::parse_from_str(&s, "%d.%m.%y").map_err(serde::de::Error::custom)
-    }
+fn infer_topic(time_options: StrptimeOptions) -> Expr {
+    when(col("Time").lt(lit("14:15:00").str().to_time(time_options.clone())))
+        .then(lit(Topic::MiTi.to_string()))
+        .when(col("Time").gt(lit("18:00:00").str().to_time(time_options)))
+        .then(lit(Topic::Verm.to_string()))
+        .otherwise(lit(Topic::LoLa.to_string()))
 }
 
-mod parse_time {
-    use chrono::NaiveTime;
-    use serde::{self, Deserialize, Deserializer, Serializer};
+fn infer_owner() -> Expr {
+    when(col("Topic").neq(lit(Topic::MiTi.to_string())))
+        .then(lit(""))
+        .when(
+            col("Description")
+                .str()
+                .contains(lit("Hauptgang"), true)
+                .or(col("Description").str().contains(lit("Kinderteller"), true))
+                .or(col("Description").str().contains(lit("Menü"), true))
+                .or(col("Description").str().contains(lit("Dessert"), true))
+                .or(col("Description").str().contains(lit("Vorspeise"), true)),
+        )
+        .then(lit(Owner::MiTi.to_string()))
+        .otherwise(lit(Owner::LoLa.to_string()))
+}
 
-    #[allow(clippy::trivially_copy_pass_by_ref)]
-    pub fn serialize<S>(date: &NaiveTime, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = format!("{}", date.format("%H:%M:%S"));
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveTime, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        NaiveTime::parse_from_str(&s, "%H:%M").map_err(serde::de::Error::custom)
-    }
+fn infer_purpose() -> Expr {
+    when(col("Description").str().contains(lit("Trinkgeld"), true))
+        .then(lit(Purpose::Tip.to_string()))
+        .otherwise(lit(Purpose::Consumption.to_string()))
 }
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
@@ -165,233 +226,62 @@ impl fmt::Display for Owner {
     }
 }
 
-/// The file format for the `Intermediate` file
-#[derive(Debug, Serialize, PartialEq)]
-#[serde(rename_all = "PascalCase")]
-pub struct Intermediate {
-    account: String,
-    #[serde(with = "parse_date")]
-    date: NaiveDate,
-    #[serde(with = "parse_time")]
-    time: NaiveTime,
-    #[serde(rename = "Type")]
-    record_type: RecordType,
-    #[serde(rename = "Transaction ID")]
-    transaction_id: String,
-    #[serde(rename = "Receipt Number")]
-    receipt_number: String,
-    #[serde(rename = "Payment Method")]
-    payment_method: Option<PaymentMethod>,
-    quantity: Option<u64>,
-    description: String,
-    currency: String,
-    #[serde(rename = "Price (Gross)")]
-    price_gross: f64,
-    #[serde(rename = "Price (Net)")]
-    price_net: Option<f64>,
-    tax: Option<f64>,
-    #[serde(rename = "Tax rate")]
-    tax_rate: Option<String>,
-    #[serde(rename = "Transaction refunded")]
-    transaction_refunded: String,
-    topic: Topic,
-    owner: Option<Owner>,
-    purpose: Purpose,
-    comment: String,
-}
-
-/// The struct matching the Sumup `SalesReport` export,
-/// e.g. 20230315-0000_20230331-2359-sales_report.csv
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct SalesReport {
-    account: String,
-    #[serde(with = "parse_date")]
-    date: NaiveDate,
-    #[serde(with = "parse_time")]
-    time: NaiveTime,
-    #[serde(rename = "Type")]
-    record_type: RecordType,
-    #[serde(rename = "Transaction ID")]
-    transaction_id: String,
-    #[serde(rename = "Receipt Number")]
-    receipt_number: String,
-    #[serde(rename = "Payment Method")]
-    payment_method: Option<PaymentMethod>,
-    quantity: Option<u64>,
-    description: String,
-    currency: String,
-    #[serde(rename = "Price (Gross)")]
-    price_gross: f64,
-    #[serde(rename = "Price (Net)")]
-    price_net: Option<f64>,
-    tax: Option<f64>,
-    #[serde(rename = "Tax rate")]
-    tax_rate: Option<String>,
-    #[serde(rename = "Transaction refunded")]
-    transaction_refunded: String,
-}
-
-impl From<SalesReport> for Intermediate {
-    fn from(ri: SalesReport) -> Self {
-        Intermediate {
-            account: ri.account,
-            date: ri.date,
-            time: ri.time,
-            record_type: ri.record_type,
-            transaction_id: ri.transaction_id,
-            receipt_number: ri.receipt_number,
-            payment_method: ri.payment_method,
-            quantity: ri.quantity,
-            description: ri.description.trim().to_string(),
-            currency: ri.currency,
-            price_gross: ri.price_gross,
-            price_net: ri.price_net,
-            tax: ri.tax,
-            tax_rate: ri.tax_rate,
-            transaction_refunded: ri.transaction_refunded,
-            topic: infer_topic(ri.time),
-            owner: infer_owner(ri.time, &ri.description),
-            purpose: infer_purpose(ri.description.trim()),
-            comment: String::new(),
-        }
-    }
-}
-
-fn infer_topic(time: NaiveTime) -> Topic {
-    let th1 = NaiveTime::from_hms_opt(14, 15, 0).unwrap();
-    let th2 = NaiveTime::from_hms_opt(18, 0, 0).unwrap();
-    if time < th1 {
-        Topic::MiTi
-    } else if time > th2 {
-        Topic::Verm
-    } else {
-        Topic::LoLa
-    }
-}
-
-fn infer_purpose(description: &str) -> Purpose {
-    if description == "Trinkgeld" {
-        Purpose::Tip
-    } else {
-        Purpose::Consumption
-    }
-}
-
-fn infer_owner(time: NaiveTime, description: &str) -> Option<Owner> {
-    let topic = infer_topic(time);
-    if topic == Topic::MiTi {
-        let patterns: Vec<&str> = vec!["Hauptgang", "Kinderteller", "Menü", "Dessert", "Vorspeise"];
-        if patterns.iter().any(|p| description.contains(p)) {
-            Some(Owner::MiTi)
-        } else {
-            Some(Owner::LoLa)
-        }
-    } else {
-        None
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use chrono::{NaiveDate, NaiveTime};
-    use csv::Reader;
+    use polars::df;
+    use polars::prelude::NamedFrom;
     use rstest::rstest;
-
-    use crate::prepare::PaymentMethod::Cash;
 
     use super::*;
 
     #[rstest]
-    #[case(
-        "09:15",
-        "Kaffee ",
-        Topic::MiTi,
-        Purpose::Consumption,
-        Some(Owner::LoLa)
-    )]
-    #[case(
-        "09:15",
-        "Hauptgang",
-        Topic::MiTi,
-        Purpose::Consumption,
-        Some(Owner::MiTi)
-    )]
-    #[case(
-        "10:59",
-        "Hauptgang+Dessert normal",
-        Topic::MiTi,
-        Purpose::Consumption,
-        Some(Owner::MiTi)
-    )]
-    #[case(
-        "11:15",
-        "Menü ganz reduziert",
-        Topic::MiTi,
-        Purpose::Consumption,
-        Some(Owner::MiTi)
-    )]
-    #[case("11:53", "Tee", Topic::MiTi, Purpose::Consumption, Some(Owner::LoLa))]
-    #[case("14:16", "Tee", Topic::LoLa, Purpose::Consumption, None)]
-    #[case("18:01", "Bier", Topic::Verm, Purpose::Consumption, None)]
-    #[case("22:59", "Trinkgeld", Topic::Verm, Purpose::Tip, None)]
-    fn test(
-        #[case] time: &str,
-        #[case] description: &str,
-        #[case] topic: Topic,
-        #[case] purpose: Purpose,
-        #[case] owner: Option<Owner>,
-    ) {
-        let csv = new_csv(time, description);
-        let expected = new_record(
-            format!("{time}:00").as_str(),
-            description.trim(),
-            topic,
-            purpose,
-            owner,
+    fn test_collect_data() {
+        let df = df!(
+            "Account" => &["a@b.ch"],
+            "Date" => &["17.04.23"],
+            "Time" => &["12:32"],
+            "Type" => &["Sales"],
+            "Transaction ID" => &["TEGUCXAGDE"],
+            "Receipt Number" => &["S20230000303"],
+            "Payment Method" => &["Cash"],
+            "Quantity" => &[1],
+            "Description" => &["foo"],
+            "Currency" => &["CHF"],
+            "Price (Gross)" => &[16.0],
+            "Price (Net)" => &[16.0],
+            "Tax" => &["0.0%"],
+            "Tax rate" => &[""],
+            "Transaction refunded" => &[""],
+        )
+        .expect("Misconfigured test data frame");
+
+        let date = NaiveDate::parse_from_str("17.4.2023", "%d.%m.%Y").expect("valid date");
+        let time = NaiveTime::parse_from_str("12:32:00", "%H:%M:%S").expect("valid time");
+        let expected = df!(
+            "Account" => &["a@b.ch"],
+            "Date" => &[date],
+            "Time" => &[time],
+            "Type" => &["Sales"],
+            "Transaction ID" => &["TEGUCXAGDE"],
+            "Receipt Number" => &["S20230000303"],
+            "Payment Method" => &["Cash"],
+            "Quantity" => &[1],
+            "Description" => &["foo"],
+            "Currency" => &["CHF"],
+            "Price (Gross)" => &[16.0],
+            "Price (Net)" => &[16.0],
+            "Tax" => &["0.0%"],
+            "Tax rate" => &[""],
+            "Transaction refunded" => &[""],
+            "Topic" => &["MiTi"],
+            "Owner" => &["LoLa"],
+            "Purpose" => &["Consumption"],
+            "Comment" => &[""],
         );
 
-        let mut reader = Reader::from_reader(csv.as_bytes());
-        let ri: SalesReport = reader.deserialize().next().unwrap().unwrap();
-        let ro: Intermediate = From::from(ri);
+        let out = build_df(&df).expect("should parse");
 
-        assert_eq!(expected, ro);
-    }
-
-    fn new_csv(time: &str, description: &str) -> String {
-        format!("\
-Account,Date,Time,Type,Transaction ID,Receipt Number,Payment Method,Quantity,Description,Currency,Price (Gross),Price (Net),Tax,Tax rate,Transaction refunded
-test@org.org,15.03.23,{time},Sales,TD4KP497FR,S20230000001,Cash,2,{description},CHF,7.00,7.00,0.00,0%,
-")
-    }
-
-    fn new_record(
-        time: &str,
-        description: &str,
-        topic: Topic,
-        purpose: Purpose,
-        owner: Option<Owner>,
-    ) -> Intermediate {
-        Intermediate {
-            account: "test@org.org".into(),
-            date: NaiveDate::from_ymd_opt(2023, 3, 15).unwrap(),
-            time: NaiveTime::parse_from_str(time, "%H:%M:%S").unwrap(),
-            record_type: RecordType::Sales,
-            transaction_id: "TD4KP497FR".into(),
-            receipt_number: "S20230000001".into(),
-            payment_method: Some(Cash),
-            quantity: Some(2),
-            description: description.into(),
-            currency: "CHF".into(),
-            price_gross: 7.0,
-            price_net: Some(7.0),
-            tax: Some(0.0),
-            tax_rate: Some("0%".into()),
-            transaction_refunded: String::new(),
-            topic,
-            purpose,
-            owner,
-            comment: String::new(),
-        }
+        assert_eq!(out, expected.expect("valid data frame"));
     }
 }
