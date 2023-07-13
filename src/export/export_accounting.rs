f44f1e2ffaa087db277c6121dba325ce84419100
Urs Joss <urs.joss@gmx.ch>
2023-07-13 20:19:49 +0200

    refactor: extract the test fixtures for the export accounting step

--- src/export/export_accounting.rs
+++ src/export/export_accounting.rs
@@ -89,78 +89,21 @@ fn validate_constraint(
 #[cfg(test)]
 #[allow(clippy::too_many_arguments)]
 mod tests {
-    use crate::test_utils::assert_dataframe;
     use chrono::NaiveDate;
     use pretty_assertions::assert_eq;
     use rstest::rstest;
 
+    use crate::test_utils::{assert_dataframe, sample_date_01, summary_df_01};
+
     use super::*;
 
-    #[test]
-    fn test_gather_df_accounting() -> PolarsResult<()> {
-        let date = NaiveDate::parse_from_str("24.3.2023", "%d.%m.%Y").expect("valid date");
-        let df_summary = df!(
-            "Date" => &[date],
-            "MiTi_Cash" => &[Some(112.0)],
-            "MiTi_Card" => &[Some(191.0)],
-            "MiTi Total" => &[303.0],
-            "Cafe_Cash" => &[Some(29.5)],
-            "Cafe_Card" => &[Some(20.0)],
-            "Cafe Total" => &[49.5],
-            "Verm_Cash" => &[Some(102.5)],
-            "Verm_Card" => &[Some(12.0)],
-            "Verm Total" => &[114.5],
-            "Deposit_Cash" => &[None::<f64>],
-            "Deposit_Card" => &[Some(100.0)],
-            "Deposit Total" => &[100.0],
-            "Rental_Cash" => &[None::<f64>],
-            "Rental_Card" => &[Some(500.0)],
-            "Rental Total" => &[500.0],
-            "Culture_Cash" => &[None::<f64>],
-            "Culture_Card" => &[400.0],
-            "Culture Total" => &[400.0],
-            "PaidOut_Cash" => &[None::<f64>],
-            "PaidOut_Card" => &[100.0],
-            "PaidOut Total" => &[100.0],
-            "Gross Cash" => &[644.0],
-            "Tips_Cash" => &[Some(4.0)],
-            "SumUp Cash" => &[248.0],
-            "Gross Card" => &[1323.0],
-            "Tips_Card" => &[Some(1.0)],
-            "SumUp Card" => &[1324.0],
-            "Gross Total" => &[1567.0],
-            "Tips Total" => &[12.5],
-            "SumUp Total" => &[1554.5],
-            "Gross Card MiTi" => &[191.0],
-            "MiTi_Commission" => &[Some(2.75)],
-            "Net Card MiTi" => &[188.25],
-            "Gross Card LoLa" => &[1132.0],
-            "LoLa_Commission" => &[17.54],
-            "LoLa_Commission_MiTi" => &[0.44],
-            "Net Card LoLa" => &[1114.46],
-            "Gross Card Total" => &[1323],
-            "Total Commission" => &[19.79],
-            "Net Card Total" => &[1303.21],
-            "Net Payment SumUp MiTi" => &[187.81],
-            "MiTi_Tips_Cash" => &[Some(0.5)],
-            "MiTi_Tips_Card" => &[Some(1.0)],
-            "MiTi_Tips" => &[Some(1.5)],
-            "Cafe_Tips" => &[Some(3.5)],
-            "Verm_Tips" => &[None::<f64>],
-            "Gross MiTi (MiTi)" => &[Some(250.0)],
-            "Gross MiTi (LoLa)" => &[Some(53.0)],
-            "Gross MiTi (MiTi) Card" => &[Some(167.0)],
-            "Net MiTi (MiTi) Card" => &[164.25],
-            "Net MiTi (LoLa)" => &[52.56],
-            "Contribution MiTi" => &[10.51],
-            "Net MiTi (LoLA) - Share LoLa" => &[52.56],
-            "Debt to MiTi" => &[145.76],
-            "Income LoLa MiTi" => &[42.49],
-            "MealCount_Regular" => &[14],
-            "MealCount_Children" => &[1],
-        )?;
+    #[rstest]
+    fn test_gather_df_accounting(
+        summary_df_01: DataFrame,
+        sample_date_01: NaiveDate,
+    ) -> PolarsResult<()> {
         let expected = df!(
-            "Date" => &[date],
+            "Date" => &[sample_date_01],
             "10920/30200" => &[20.0],
             "10920/30700" => &[12.0],
             "10920/23050" => &[100.0],
@@ -175,7 +118,7 @@ mod tests {
             "20051/30500" => &[42.49],
         )?;
         let out =
-            gather_df_accounting(&df_summary).expect("should be able to collect accounting_df");
+            gather_df_accounting(&summary_df_01).expect("should be able to collect accounting_df");
 
         assert_dataframe(&out, &expected);
 
--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDate;
 use polars::df;
 use polars::frame::DataFrame;
 use polars::prelude::{AnyValue, NamedFrom};
@@ -41,7 +42,7 @@ pub fn sales_report_df_01() -> DataFrame {
         "Tax rate" => &[""],
         "Transaction refunded" => &[""],
     )
-    .expect("valid dataframe")
+    .expect("valid dataframe sales report data frame 01")
 }
 
 /// Sample record matching the structure of the sumup transaction report csv file
@@ -55,7 +56,7 @@ pub fn transaction_report_df_01() -> DataFrame {
         "Trinkgeldbetrag" => &[1.0],
         "Gebühr" => &[0.24],
     )
-    .expect("valid dataframe")
+    .expect("valid dataframe transaction report data frame 01")
 }
 
 /// Sample record matching the structure of the intermediate csv file
@@ -83,5 +84,76 @@ pub fn intermediate_df_01() -> DataFrame {
         "Purpose" => &["Consumption", "Tip"],
         "Comment" => &[AnyValue::Null, AnyValue::Null],
     )
-    .expect("valid dataframe")
+    .expect("valid intermediate dataframe 01")
+}
+
+#[fixture]
+pub fn sample_date_01() -> NaiveDate {
+    NaiveDate::parse_from_str("24.3.2023", "%d.%m.%Y").expect("valid date")
+}
+
+/// Sample record matching the summary dataframe created from the intermediate csv file
+#[fixture]
+pub fn summary_df_01(sample_date_01: NaiveDate) -> DataFrame {
+    df!(
+        "Date" => &[sample_date_01],
+        "MiTi_Cash" => &[Some(112.0)],
+        "MiTi_Card" => &[Some(191.0)],
+        "MiTi Total" => &[303.0],
+        "Cafe_Cash" => &[Some(29.5)],
+        "Cafe_Card" => &[Some(20.0)],
+        "Cafe Total" => &[49.5],
+        "Verm_Cash" => &[Some(102.5)],
+        "Verm_Card" => &[Some(12.0)],
+        "Verm Total" => &[114.5],
+        "Deposit_Cash" => &[None::<f64>],
+        "Deposit_Card" => &[Some(100.0)],
+        "Deposit Total" => &[100.0],
+        "Rental_Cash" => &[None::<f64>],
+        "Rental_Card" => &[Some(500.0)],
+        "Rental Total" => &[500.0],
+        "Culture_Cash" => &[None::<f64>],
+        "Culture_Card" => &[400.0],
+        "Culture Total" => &[400.0],
+        "PaidOut_Cash" => &[None::<f64>],
+        "PaidOut_Card" => &[100.0],
+        "PaidOut Total" => &[100.0],
+        "Gross Cash" => &[644.0],
+        "Tips_Cash" => &[Some(4.0)],
+        "SumUp Cash" => &[248.0],
+        "Gross Card" => &[1323.0],
+        "Tips_Card" => &[Some(1.0)],
+        "SumUp Card" => &[1324.0],
+        "Gross Total" => &[1567.0],
+        "Tips Total" => &[12.5],
+        "SumUp Total" => &[1554.5],
+        "Gross Card MiTi" => &[191.0],
+        "MiTi_Commission" => &[Some(2.75)],
+        "Net Card MiTi" => &[188.25],
+        "Gross Card LoLa" => &[1132.0],
+        "LoLa_Commission" => &[17.54],
+        "LoLa_Commission_MiTi" => &[0.44],
+        "Net Card LoLa" => &[1114.46],
+        "Gross Card Total" => &[1323],
+        "Total Commission" => &[19.79],
+        "Net Card Total" => &[1303.21],
+        "Net Payment SumUp MiTi" => &[187.81],
+        "MiTi_Tips_Cash" => &[Some(0.5)],
+        "MiTi_Tips_Card" => &[Some(1.0)],
+        "MiTi_Tips" => &[Some(1.5)],
+        "Cafe_Tips" => &[Some(3.5)],
+        "Verm_Tips" => &[None::<f64>],
+        "Gross MiTi (MiTi)" => &[Some(250.0)],
+        "Gross MiTi (LoLa)" => &[Some(53.0)],
+        "Gross MiTi (MiTi) Card" => &[Some(167.0)],
+        "Net MiTi (MiTi) Card" => &[164.25],
+        "Net MiTi (LoLa)" => &[52.56],
+        "Contribution MiTi" => &[10.51],
+        "Net MiTi (LoLA) - Share LoLa" => &[52.56],
+        "Debt to MiTi" => &[145.76],
+        "Income LoLa MiTi" => &[42.49],
+        "MealCount_Regular" => &[14],
+        "MealCount_Children" => &[1],
+    )
+    .expect("valid summary dataframe 01")
 }
