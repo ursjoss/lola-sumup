d0c79ec3b082bcb38f3888c61180f5b34c93a8e2
Urs Joss <urs.joss@gmx.ch>
2023-07-13 20:23:57 +0200

    refactor: extract the test fixtures for the export miti step

--- src/export/export_miti.rs
+++ src/export/export_miti.rs
@@ -72,76 +72,19 @@ pub fn gather_df_miti(df: &DataFrame) -> PolarsResult<DataFrame> {
 #[cfg(test)]
 mod tests {
     use chrono::NaiveDate;
+    use rstest::rstest;
 
-    use crate::test_utils::assert_dataframe;
+    use crate::test_utils::{assert_dataframe, sample_date_01, summary_df_02};
 
     use super::*;
 
-    #[test]
-    fn test_gather_df_miti() -> PolarsResult<()> {
-        let date = NaiveDate::parse_from_str("24.3.2023", "%d.%m.%Y").expect("valid date");
-        let df_summary = df!(
-            "Date" => &[date],
-            "MiTi_Cash" => &[Some(112.0)],
-            "MiTi_Card" => &[Some(191.0)],
-            "MiTi Total" => &[303.0],
-            "Cafe_Cash" => &[Some(29.5)],
-            "Cafe_Card" => &[Some(20)],
-            "Cafe Total" => &[49.5],
-            "Verm_Cash" => &[Some(102.5)],
-            "Verm_Card" => &[Some(12.0)],
-            "Verm Total" => &[114.5],
-            "Deposit_Cash" => &[None::<f64>],
-            "Deposit_Card" => &[Some(100.0)],
-            "Deposit Total" => &[100.0],
-            "Rental_Cash" => &[None::<f64>],
-            "Rental_Card" => &[Some(500.0)],
-            "Rental Total" => &[500.0],
-            "Culture_Cash" => &[None::<f64>],
-            "Culture_Card" => &[400.0],
-            "Culture Total" => &[400.0],
-            "PaidOut_Cash" => &[None::<f64>],
-            "PaidOut_Card" => &[100.0],
-            "PaidOut Total" => &[100.0],
-            "Gross Cash" => &[644.0],
-            "Tips_Cash" => &[Some(4.0)],
-            "SumUp Cash" => &[248.0],
-            "Gross Card" => &[1323.0],
-            "Tips_Card" => &[Some(1.0)],
-            "SumUp Card" => &[1324.0],
-            "Gross Total" => &[1567.0],
-            "Tips Total" => &[5.0],
-            "SumUp Total" => &[1572.0],
-            "Gross Card MiTi" => &[191.0],
-            "MiTi_Commission" => &[Some(2.75)],
-            "Net Card MiTi" => &[188.25],
-            "Gross Card LoLa" => &[1132.0],
-            "LoLa_Commission" => &[17.54],
-            "LoLa_Commission_MiTi" => &[0.44],
-            "Net Card LoLa" => &[218.96],
-            "Gross Card Total" => &[1115.56],
-            "Total Commission" => &[20.29],
-            "Net Card Total" => &[1065.27],
-            "Net Payment SumUp MiTi" => &[187.81],
-            "MiTi_Tips_Cash" => &[Some(0.5)],
-            "MiTi_Tips_Card" => &[Some(1.0)],
-            "MiTi_Tips" => &[Some(1.5)],
-            "Cafe_Tips" => &[Some(3.5)],
-            "Verm_Tips" => &[None::<f64>],
-            "Gross MiTi (MiTi)" => &[Some(250.0)],
-            "Gross MiTi (LoLa)" => &[Some(53.0)],
-            "Gross MiTi (MiTi) Card" => &[Some(167.0)],
-            "Net MiTi (MiTi) Card" => &[164.25],
-            "Net MiTi (LoLa)" => &[52.56],
-            "Contribution MiTi" => &[10.51],
-            "Net MiTi (LoLA) - Share LoLa" => &[42.05],
-            "Debt to MiTi" => &[145.76],
-            "Income LoLa MiTi" => &[42.49],
-            "MealCount_Regular" => &[14],
-            "MealCount_Children" => &[1],
-        )?;
+    #[rstest]
+    fn test_gather_df_miti(
+        summary_df_02: DataFrame,
+        sample_date_01: NaiveDate,
+    ) -> PolarsResult<()> {
         let expected = df!(
-            "Datum" => &[date],
+            "Datum" => &[sample_date_01],
             "Hauptgang" => &[14],
             "Kind" => &[1],
             "Küche" => &[Some(250.0)],
@@ -162,7 +105,7 @@ mod tests {
             "Verkauf LoLa (80%)" => &[-42.05],
             "Überweisung" => &[145.76],
         )?;
-        let out = gather_df_miti(&df_summary).expect("should be able to collect miti_df");
+        let out = gather_df_miti(&summary_df_02).expect("should be able to collect miti_df");
 
         assert_dataframe(&out, &expected);
 
