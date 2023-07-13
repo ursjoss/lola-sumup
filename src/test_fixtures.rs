2a003646edb5859700e99f893bdd68601a84f499
Urs Joss <urs.joss@gmx.ch>
2023-07-13 21:41:42 +0200

    Unify summary df for accounting and miti

--- src/test_fixtures.rs
+++ src/test_fixtures.rs
@@ -183,75 +183,9 @@ pub fn summary_df_02(sample_date: NaiveDate) -> DataFrame {
 
 /// Sample record 03 matching the summary dataframe created from the intermediate csv file
 /// accounting
-#[fixture]
-pub fn summary_df_03(sample_date: NaiveDate) -> DataFrame {
-    df!(
-        "Date" => &[sample_date],
-        "MiTi_Cash" => &[Some(112.0)],
-        "MiTi_Card" => &[Some(191.0)],
-        "MiTi Total" => &[303.0],
-        "Cafe_Cash" => &[Some(29.5)],
-        "Cafe_Card" => &[Some(20.0)],
-        "Cafe Total" => &[49.5],
-        "Verm_Cash" => &[Some(102.5)],
-        "Verm_Card" => &[Some(12.0)],
-        "Verm Total" => &[114.5],
-        "Deposit_Cash" => &[None::<f64>],
-        "Deposit_Card" => &[Some(100.0)],
-        "Deposit Total" => &[100.0],
-        "Rental_Cash" => &[None::<f64>],
-        "Rental_Card" => &[Some(500.0)],
-        "Rental Total" => &[500.0],
-        "Culture_Cash" => &[None::<f64>],
-        "Culture_Card" => &[400.0],
-        "Culture Total" => &[400.0],
-        "PaidOut_Cash" => &[None::<f64>],
-        "PaidOut_Card" => &[100.0],
-        "PaidOut Total" => &[100.0],
-        "Gross Cash" => &[644.0],
-        "Tips_Cash" => &[Some(4.0)],
-        "SumUp Cash" => &[248.0],
-        "Gross Card" => &[1323.0],
-        "Tips_Card" => &[Some(1.0)],
-        "SumUp Card" => &[1324.0],
-        "Gross Total" => &[1567.0],
-        "Tips Total" => &[5.0],
-        "SumUp Total" => &[1572.0],
-        "Gross Card MiTi" => &[191.0],
-        "MiTi_Commission" => &[Some(2.75)],
-        "Net Card MiTi" => &[188.25],
-        "Gross Card LoLa" => &[1132.0],
-        "LoLa_Commission" => &[17.54],
-        "LoLa_Commission_MiTi" => &[0.44],
-        "Net Card LoLa" => &[1114.46],
-        "Gross Card Total" => &[1323],
-        "Total Commission" => &[19.79],
-        "Net Card Total" => &[1303.21],
-        "Net Payment SumUp MiTi" => &[187.81],
-        "MiTi_Tips_Cash" => &[Some(0.5)],
-        "MiTi_Tips_Card" => &[Some(1.0)],
-        "MiTi_Tips" => &[Some(1.5)],
-        "Cafe_Tips" => &[Some(3.5)],
-        "Verm_Tips" => &[None::<f64>],
-        "Gross MiTi (MiTi)" => &[Some(250.0)],
-        "Gross MiTi (LoLa)" => &[Some(53.0)],
-        "Gross MiTi (MiTi) Card" => &[Some(167.0)],
-        "Net MiTi (MiTi) Card" => &[164.25],
-        "Net MiTi (LoLa)" => &[52.56],
-        "Contribution MiTi" => &[10.51],
-        "Net MiTi (LoLA) - Share LoLa" => &[42.05],
-        "Debt to MiTi" => &[145.76],
-        "Income LoLa MiTi" => &[42.49],
-        "MealCount_Regular" => &[14],
-        "MealCount_Children" => &[1],
-    )
-    .expect("valid summary dataframe 01")
-}
-
-/// Sample record 02 matching the summary dataframe created from the intermediate csv file
 /// miti
 #[fixture]
-pub fn summary_df_04(sample_date: NaiveDate) -> DataFrame {
+pub fn summary_df_03(sample_date: NaiveDate) -> DataFrame {
     df!(
         "Date" => &[sample_date],
         "MiTi_Cash" => &[Some(112.0)],
