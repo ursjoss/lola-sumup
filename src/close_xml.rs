4af2df51806d2e60797f0e01b94a3b4334b024a8
Urs Joss <urs.joss@gmx.ch>
2024-12-13 23:08:07 +0100

    Read data from xml file called xls

--- src/close_xml.rs
+++ src/close_xml.rs
@@ -1,3 +1,4 @@
+use polars::prelude::*;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
 use std::collections::HashMap;
@@ -6,11 +7,6 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
-//Row, ss:Index
-//- 10: Account
-//- 11: Description
-//- 21: Debit
-//- 22: Credit
 pub fn do_closing_xls(input_path: &Path, _month: &str, _ts: &str) -> Result<(), Box<dyn Error>> {
     let file = BufReader::new(File::open(input_path)?);
     let mut reader = Reader::from_reader(file);
@@ -22,14 +18,17 @@ pub fn do_closing_xls(input_path: &Path, _month: &str, _ts: &str) -> Result<(), 
     let mut in_cell = false;
     let mut cell_value = String::new();
     let mut index: Option<u32> = None;
-    let mut row = HashMap::new();
+    let mut row: HashMap<u32, String> = HashMap::new();
+    let mut df = new_empty_frame()?;
 
     loop {
         match reader.read_event_into(&mut buf) {
             Ok(Event::Start(ref e)) => match e.name().as_ref() {
                 b"Worksheet" => {
                     for attr in e.attributes().flatten() {
-                        if attr.key.as_ref() == b"ss:Name" && attr.unescape_value()? == "Accounts" {
+                        if attr.key.as_ref() == b"ss:Name"
+                            && attr.unescape_value()? == Sheet::Accounts.name()
+                        {
                             in_sheet = true;
                         }
                     }
@@ -61,7 +60,28 @@ pub fn do_closing_xls(input_path: &Path, _month: &str, _ts: &str) -> Result<(), 
                 }
                 b"Row" => {
                     if in_sheet {
-                        println!("--- {row:?}");
+                        let mut df_row = new_row(
+                            row.remove(&10).unwrap_or("account missing".into()),
+                            row.remove(&11).unwrap_or("description missing".into()),
+                            &row.remove(&21).unwrap_or("0.0".into()),
+                            &row.remove(&22).unwrap_or("0.0".into()),
+                        )?;
+                        df_row = df_row.fill_null(FillNullStrategy::Zero)?;
+                        let filtered = df_row
+                            .clone()
+                            .lazy()
+                            .filter(
+                                col("Account").is_not_null().and(
+                                    col("Account")
+                                        .str()
+                                        .contains(lit(r"^[3456789]\d{3,4}$"), false),
+                                ),
+                            )
+                            .select([col("Account")])
+                            .collect()?;
+                        if filtered.shape().0 > 0 {
+                            df = df.vstack(&df_row)?;
+                        }
                         row.clear();
                     }
                 }
@@ -79,5 +99,98 @@ pub fn do_closing_xls(input_path: &Path, _month: &str, _ts: &str) -> Result<(), 
         buf.clear();
     }
 
+    println!("{df:?}");
     Ok(())
+}
+
+fn new_row(
+    account: String,
+    description: String,
+    debit: &str,
+    credit: &str,
+) -> Result<DataFrame, Box<dyn Error>> {
+    let debit_numeric: f64 = debit.parse().unwrap_or(0.0);
+    let credit_numeric: f64 = credit.parse().unwrap_or(0.0);
+    new_row_with_vecs(
+        vec![account],
+        vec![description],
+        vec![debit_numeric],
+        vec![credit_numeric],
+    )
+}
+
+fn new_empty_frame() -> Result<DataFrame, Box<dyn Error>> {
+    new_row_with_vecs(
+        Vec::<String>::new(),
+        Vec::<String>::new(),
+        Vec::<f64>::new(),
+        Vec::<f64>::new(),
+    )
+}
+
+fn new_row_with_vecs(
+    account: Vec<String>,
+    description: Vec<String>,
+    debit: Vec<f64>,
+    credit: Vec<f64>,
+) -> Result<DataFrame, Box<dyn Error>> {
+    let df = DataFrame::new(vec![
+        Column::new(
+            AccountsColumn::Account.name().into(),
+            Series::new(AccountsColumn::Account.name().into(), account),
+        ),
+        Column::new(
+            AccountsColumn::Description.name().into(),
+            Series::new(AccountsColumn::Description.name().into(), description),
+        ),
+        Column::new(
+            AccountsColumn::Debit.name().into(),
+            Series::new(AccountsColumn::Debit.name().into(), debit),
+        ),
+        Column::new(
+            AccountsColumn::Credit.name().into(),
+            Series::new(AccountsColumn::Credit.name().into(), credit),
+        ),
+    ])?;
+    Ok(df)
+}
+
+/// The worksheets in the workbook
+#[derive(Debug, Clone, Copy)]
+enum Sheet {
+    Accounts = 0,
+    _Totals = 1,
+    _Journal = 2,
+    _FileInfo = 3,
+}
+
+impl Sheet {
+    fn name(self) -> &'static str {
+        match self {
+            Sheet::Accounts => "Accounts",
+            Sheet::_Totals => "Totals",
+            Sheet::_Journal => "Journal",
+            Sheet::_FileInfo => "FileInfo",
+        }
+    }
+}
+
+/// The columns in the worksheet Accounts, index is one-based
+#[derive(Debug, Clone, Copy)]
+enum AccountsColumn {
+    Account = 10,
+    Description = 11,
+    Debit = 21,
+    Credit = 22,
+}
+
+impl AccountsColumn {
+    fn name(self) -> &'static str {
+        match self {
+            AccountsColumn::Account => "Account",
+            AccountsColumn::Description => "Description",
+            AccountsColumn::Debit => "Debit",
+            AccountsColumn::Credit => "Credit",
+        }
+    }
 }
